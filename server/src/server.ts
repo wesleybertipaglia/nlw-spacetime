ece74de8c6ecc3bf2cea6afc8cfd76436c4ff776
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-05-20 20:45:23 -0300

    Create front-end and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,17 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
+
+app.register(cors, {
+  origin: ['http://localhost:3000'],
+})
+
+app.register(memoriesRoutes)
 
 app.get('/', () => {
   return 'Hello, world'
-})
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
 })
 
 app
