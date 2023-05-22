7592f4da8c60a7512c4bc1971827e72e20039220
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-05-21 21:46:15 -0300

    Add File upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
+import { resolve } from 'node:path'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 
@@ -13,9 +16,15 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime',
 })
+app.register(multipart)
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
 
 app
   .listen({
